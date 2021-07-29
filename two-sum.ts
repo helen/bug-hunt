998a9f0fc406047aad98fdff6363d3c88ee8f384
Helen Hou-Sandi <helen.y.hou@gmail.com>
2021-07-29 16:06:09 -0600

    Explanatory link and whitespace tweaks

--- two-sum.ts
+++ two-sum.ts
@@ -1,18 +1,18 @@
+// See https://leetcode.com/problems/two-sum/
+
 function twoSum(nums: number[], target: number): number[] {
     let others: number[] = nums;
 
     for( let i = 0; i < nums.length; i++ ) {
         others.shift();
-        
+
         let remainder: number = target - nums[i];
-        
         let key2: number = others.indexOf( remainder );
-        
+
         if ( -1 !== key2 ) {
             return [ i, key2 + i + 1 ];
         }
     }
-    
+
     return [];
 };
-                                    
